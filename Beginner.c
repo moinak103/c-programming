4eb706df375843bc79a8b7bb4b0b86ba9fb4d8eb
moinak103 <moinak103@gmail.com>
2017-12-09 20:54:09 +0530

    writing my first program

---
+++ Beginner.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+  
+    int age = 18;
+    float weight = 67.8;
+    char gender = 'M';
+
+  printf("My name is Moinak Moitra \t My age is %d\n My weight is %f \t My gender is %c",age,weight,gender);
+
+   getch();
+   return 0;
+}
---
+++ ScanF.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+    int main()
+{
+
+      int age;
+      int weight;
+
+   
+
+   	  printf("Please enter your age and weight\n");
+
+      scanf("%d%d",&age,&weight);
+
+      printf("Your age is %d\n Your weight is %d",age,weight);
+
+      getch();
+      return 0;
+
+
+}    
