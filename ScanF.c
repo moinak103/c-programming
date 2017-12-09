57e524fe32d564362d50782af919eeada77607a9
moinak103 <moinak103@gmail.com>
2017-12-09 21:03:44 +0530

    adding comments to my program

--- ScanF.c
+++ ScanF.c
@@ -3,16 +3,18 @@
 
     int main()
 {
-
+		// declaring two variables
       int age;
       int weight;
 
    
-
+      // asking user to enter details
    	  printf("Please enter your age and weight\n");
 
+   	  // getting users details
       scanf("%d%d",&age,&weight);
 
+      // printing user details
       printf("Your age is %d\n Your weight is %d",age,weight);
 
       getch();
